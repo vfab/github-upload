57f1c79849c2403ea1acee5f30b5df456ddea7e0
Vince Fabro <vince.fabro@techdata.com>
2020-06-04 18:16:29 -0700

    Initial commit

---
+++ ToDoService/Controllers/ToDoController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : Controller
+    {
+        // GET api/todo
+        [HttpGet]
+        public IEnumerable<Model.ToDoItem> Get()
+        {
+            return DataContext.ToDoList.Items;
+        }
+
+        // GET api/todo/5
+        [HttpGet("{index}")]
+        public Model.ToDoItem Get(int index)
+        {
+            return DataContext.ToDoList.Items.ElementAt(index);
+        }
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        // DELETE api/values/5
+        [HttpDelete("{index}")]
+        public void Delete(int index)
+        {
+        }
+    }
+}
---
+++ WebFrontEnd/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebFrontEnd.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public Model.ToDoItem[] Items = new Model.ToDoItem[] { };
+
+        private static string backendDNSName = $"{Environment.GetEnvironmentVariable("ToDoServiceName")}";
+        private static Uri backendUrl = new Uri($"http://{backendDNSName}:{Environment.GetEnvironmentVariable("ApiHostPort")}/api/todo");
+
+        public void OnGet()
+        {
+            HttpClient client = new HttpClient();
+
+            var variables = Environment.GetEnvironmentVariables();
+
+            using (HttpResponseMessage response = client.GetAsync(backendUrl).GetAwaiter().GetResult())
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    Items = Newtonsoft.Json.JsonConvert.DeserializeObject<Model.ToDoItem[]>(response.Content.ReadAsStringAsync().Result);
+                }
+            }
+        }
+    }
+}
